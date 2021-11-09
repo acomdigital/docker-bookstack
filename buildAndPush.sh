6e99cc36d5a049a15f118a4ea53314f8c7667a3a
Yaroslav Safarov <safarov.yaroslav@gmail.com>
2021-11-09 18:18:45 +0200

    upgrade bookstack to v21.10.3

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull php:7.4-apache-buster
-docker build -t marcode79/bookstack:21.05.2-ee .
-docker push marcode79/bookstack:21.05.2-ee
+docker build -t marcode79/bookstack:21.10.3-ee .
+docker push marcode79/bookstack:21.10.3-ee
